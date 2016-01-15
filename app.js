de509b15fa478cc10323d863e6ee99707712966f
Gary Pang <gary.pang@gmail.com>
2016-01-15 18:38:18 -0500

    Update app.js
    Remove $firebase, and change $set( ) to set( ). Original tutorial uses
    the $firebase service, which has been removed from AngularFire since
    version 1.x.

--- app.js
+++ app.js
@@ -2,10 +2,43 @@
 angular.module('scheduleApp', ['firebase'])
 
 // Create main controller and get access to Firebase
-.controller('mainController', function ($scope, $firebase) {
+.controller('mainController', function ($scope) {
 
     // Connect to Firebase
     var ref = new Firebase("https://cow-scheduling-app.firebaseio.com/days");
-    var fb = $firebase(ref);
+
+    // Function to set default data
+    $scope.reset = function () {
+
+        ref.set({
+            monday: {
+                name: 'Monday',
+                slots: {
+                    0900: {
+                        time: '9 a.m.',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11 a.m.',
+                        booked: false
+                    }
+                }
+            },
+            tuesday: {
+                name: 'Tuesday',
+                slots: {
+                    0900: {
+                        time: '9 a.m.',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11 a.m.',
+                        booked: false
+                    }
+                }
+            }
+        });
+
+    };
 
 });
