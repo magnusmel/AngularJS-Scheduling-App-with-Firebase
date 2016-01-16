f685ec83d13865d9b2d0b2e38429f7bd22d0cf34
Gary Pang <gary.pang@gmail.com>
2016-01-15 23:24:02 -0500

    Update app.js
    Inject $firebaseObject into mainController. Change "monday-friday" to
    "day1-5," so Firebase now sorts days chronologically, not
    alphabetically. Edit README.md to explain how to migrate app from older
    versions of AngularFire to 1.x.

--- README.md
+++ README.md
@@ -3,3 +3,38 @@ _by Gary Pang, "CodeWritingCow"_
 
 This is a real-time scheduling app made with AngularJS and Firebase.
 From the Scotch.io tutorial, ["Build a Real-Time Scheduling App using AngularJS and Firebase,"](https://scotch.io/tutorials/build-a-real-time-scheduling-app-using-angularjs-and-firebase) by Chris Sevilleja.
+
+
+## Changes since AngularFire 1.x
+The tutorial uses AngularFire 0.8.0, which includes the $firebase service and $set() method. Both have been removed from AngularFire since version 1.x, so I have rewritten my app accordingly. Below are snippets of codes from the original app, followed by my updated one.
+
+
+##### Original
+```javascript
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+  
+  // connect to firebase 
+  var ref = new Firebase("https://xxxxx.firebaseio.com/days");  
+  var fb = $firebase(ref);
+
+  // sync as object 
+  var syncObject = fb.$asObject();
+
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
+  ```
+
+
+##### Updated
+```javascript
+.controller('mainController', function ($scope, $firebaseObject) {
+
+    var ref = new Firebase("https://xxxxx.firebaseio.com/days");
+
+    var syncObject = $firebaseObject(ref);
+
+    syncObject.$bindTo($scope, 'days');
+```
+
+Read more about changes since AngularFire 1.x in Firebase's [migration guides.](https://www.firebase.com/docs/web/libraries/angular/migration-guides.html)
--- app.js
+++ app.js
@@ -2,16 +2,22 @@
 angular.module('scheduleApp', ['firebase'])
 
 // Create main controller and get access to Firebase
-.controller('mainController', function ($scope) {
+.controller('mainController', function ($scope, $firebaseObject) {
 
     // Connect to Firebase
     var ref = new Firebase("https://cow-scheduling-app.firebaseio.com/days");
+
+    // Sync as object. Original tutorial uses $firebase and $asObject(). 
+    var syncObject = $firebaseObject(ref);
+
+    // Three-way data binding
+    syncObject.$bindTo($scope, 'days');
 
     // Function to set default data
     $scope.reset = function () {
 
         ref.set({
-            monday: {
+            day1: {
                 name: 'Monday',
                 slots: {
                     0900: {
@@ -24,7 +30,7 @@ angular.module('scheduleApp', ['firebase'])
                     }
                 }
             },
-            tuesday: {
+            day2: {
                 name: 'Tuesday',
                 slots: {
                     0900: {
@@ -36,7 +42,47 @@ angular.module('scheduleApp', ['firebase'])
                         booked: false
                     }
                 }
+            },
+            day3: {
+                name: 'Wednesday',
+                slots: {
+                    0900: {
+                        time: '9 a.m.',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11 a.m.',
+                        booked: false
+                    }
+                }
+            },
+            day4: {
+                name: 'Thursday',
+                slots: {
+                    0900: {
+                        time: '9 a.m.',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11 a.m.',
+                        booked: false
+                    }
+                }
+            },
+            day5: {
+                name: 'Friday',
+                slots: {
+                    0900: {
+                        time: '9 a.m.',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11 a.m.',
+                        booked: false
+                    }
+                }
             }
+
         });
 
     };
